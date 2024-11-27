952a232944a7afc085ab0fed86bec6df75823c64
Anton Saladounikau <lighthousekeeperyn@gmail.com>
2024-11-27 12:19:32 +0100

    fix: disable chat when user is not authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Chat from '@/components/chat';
 import UserSwitch from '@/components/user-switch';
+import { createClient } from '@/utils/supabase/server';
 
 export default async function Index() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
       <UserSwitch />
-      <Chat />
+      {user && <Chat />}
     </main>
   );
 }
